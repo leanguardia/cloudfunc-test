e41fc8bf90affde612d905f604cf19e9d520f49c
D. Leandro Guardia V <lean.guardia93@gmail.com>
2018-10-02 19:32:57 -0400

    Hello World, addMessage and callback on onCreate

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,26 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((req, res) => {
+ res.send("Hello from Firebase!");
+});
+
+export const addMessage = functions.https.onRequest((req, res) => {
+    const original = req.query.text;
+    admin.database().ref('/messages').push({original: original}).then((snapshot) => {
+        res.redirect(303, snapshot.ref.toString());
+    });
+});
+
+export const makeUppercase = functions.database.ref('/messages/{pushId}/original')
+    .onCreate((snapshot, context) => {
+        const original = snapshot.val();
+        console.log('Uppercasing', context.params.pushId, original);
+        const uppercase = original.toUpperCase();
+        return snapshot.ref.parent.child('uppercase').set(uppercase);
+    });
